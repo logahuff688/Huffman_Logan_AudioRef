5a5bea533be9076554c77bc5b2c703a0d297b4bd
logahuff688 <113370990+logahuff688@users.noreply.github.com>
2023-02-27 17:39:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//coolect all of the album covers into a variable
+//collect all of the album covers into a variable
 
 const   albumcovers = document.querySelectorAll("#album-art img"),
         theAudioEl = document.querySelector('audio'),
@@ -10,7 +10,7 @@ const   albumcovers = document.querySelectorAll("#album-art img"),
 
 
 
-//tell the audio elemet to play
+//tell the audio element to play
 
 function loadAudio() {
     let currentSrc =`audio/${this.dataset.trackref}.mp3`;
@@ -18,7 +18,7 @@ function loadAudio() {
     //set a new audio source
     theAudioEl.src=currentSrc;
 
-    //load the nnew audio source
+    //load the new audio source
     theAudioEl.load;
 
 
@@ -26,7 +26,7 @@ function loadAudio() {
     playAudio();
 
 }
-//load  a new audio sorce to start play
+//load  a new audio source to start play
 
 
 
@@ -37,22 +37,22 @@ function pauseAudio(){ theAudioEl.pause();}
 
 
 function setVolume(){
-    //get the nnumeric value of the volume slider between 0 min and 100 max
+    //get the numeric value of the volume slider between 0 min and 100 max
     //that's what the volume of the audio should be set to 10
 
     console.log(this.value);
 
-    //divide the value by 100 to get q floating poinnt between o annnd 1 -> .5 .85 etc
+    //divide the value by 100 to get q floating point between o and 1 -> .5 .85 etc
     theAudioEl.volume = (this.value/100);
 }
 
 
 
-//add  event hanndlinng to the album covers (listen for a click event)
+//add  event handling to the album covers (listen for a click event)
 
 albumcovers.forEach(cover => cover.addEventListener('click',loadAudio));
 
-//add event handeling for the custom controls
+//add event handling for the custom controls
 playbutton.addEventListener('click', playAudio);
 rewindbutton.addEventListener('click', restartAudio);
 stopbutton.addEventListener('click', pauseAudio)
