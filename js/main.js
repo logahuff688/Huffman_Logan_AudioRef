0459f7d847d0afea5a7b4b6f6835e38b45b5d2d5
logahuff688 <113370990+logahuff688@users.noreply.github.com>
2023-02-27 15:48:24 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-// create a module for our project
-(() => {
+ // collect all of the album covers into a variable
+ const albumCovers = document.querySelectorAll("#album-art img");
+        theAudioEl = document.querySelector("audio");
 
-})()
+ // tell the audio element to play
+ function playAudio() {
+    // debug (test) your code at this point before you add anything else
+    debugger;
+ }
+
+ // add event handling to the album covers (listen for a click event)
+ albumCovers.forEach(cover => cover.addEventListener('click', playAudio))
